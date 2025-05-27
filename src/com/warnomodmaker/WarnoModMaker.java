0665fa9061e1e3e057fdba4a8abf6b0ddb4c6c58
samy109 <adamhumphries5@gmail.com>
2025-05-27 09:41:50 -0500

    implement flatlaf

--- src/com/warnomodmaker/WarnoModMaker.java
+++ src/com/warnomodmaker/WarnoModMaker.java
@@ -1,5 +1,6 @@
 package com.warnomodmaker;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.warnomodmaker.gui.MainWindow;
 
 import javax.swing.*;
@@ -10,7 +11,7 @@ public class WarnoModMaker {
     public static void main(String[] args) {
 
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            FlatDarkLaf.setup();
         } catch (Exception e) {
             e.printStackTrace();
         }
